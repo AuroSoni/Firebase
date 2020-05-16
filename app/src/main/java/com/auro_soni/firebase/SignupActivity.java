e9a178d9f0b6a295ead692e94ad0285a117a84b3
Auro <aurokumar926@gmail.com>
2020-05-16 09:52:35 +0530

    layouts configured

---
+++ app/src/main/java/com/auro_soni/firebase/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.auro_soni.firebase;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
